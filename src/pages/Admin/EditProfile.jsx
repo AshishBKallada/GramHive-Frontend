e3f11e8a128520429c01060453fdcb5507af3606
ASHISH B KALLADA <ashishbkallada@gmail.com>
2024-06-28 17:11:36 +0530

    sidebar modified 2

--- src/pages/Admin/EditProfile.jsx
+++ src/pages/Admin/EditProfile.jsx
@@ -4,14 +4,15 @@ import SidebarTest from "../../Components/User/Sidebar/SidebarTest";
 
 function EditUserProfile() {
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start">
-      <div className="lg:max-w-[300px] w-full lg:w-auto">
-        <SidebarTest />
-      </div>
-      <div className="flex-1 lg:ml-4 mt-8 lg:mt-0">
-        <EditProfile />
-      </div>
+    <div>
+    <div className="float-left fixed">
+      <SidebarTest />
     </div>
+
+    <div style={{ marginLeft: "200px" }}>
+      <EditProfile />
+    </div>
+  </div>
   );
 }
 
