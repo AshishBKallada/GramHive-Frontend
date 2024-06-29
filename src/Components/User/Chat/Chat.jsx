6c14720e6de07806c0e6a9573e658c0c5f8fa536
ASHISH B KALLADA <ashishbkallada@gmail.com>
2024-06-29 12:46:35 +0530

    chat interface modified!

--- src/Components/User/Chat/Chat.jsx
+++ src/Components/User/Chat/Chat.jsx
@@ -302,14 +302,14 @@ const Chat = () => {
                   ? getSender(userId, selectedChat.users)
                   : selectedChat.latestMessage?.sender.username}
               </p>
-              <p className="text-grey-darker text-xs mt-1">
+              {/* <p className="text-grey-darker text-xs mt-1">
                 Andrés, Tom, Harrison, Arnold, Sylvester
-              </p>
+              </p> */}
             </div>
           </div>
 
           <div className="flex">
-            <div>
+            {/* <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -322,7 +322,7 @@ const Chat = () => {
                   d="M15.9 14.3H15l-.3-.3c1-1.1 1.6-2.7 1.6-4.3 0-3.7-3-6.7-6.7-6.7S3 6 3 9.7s3 6.7 6.7 6.7c1.6 0 3.2-.6 4.3-1.6l.3.3v.8l5.1 5.1 1.5-1.5-5-5.2zm-6.2 0c-2.6 0-4.6-2.1-4.6-4.6s2.1-4.6 4.6-4.6 4.6 2.1 4.6 4.6-2 4.6-4.6 4.6z"
                 ></path>
               </svg>
-            </div>
+            </div> */}
             <div className="ml-6">
               <button onClick={handleCall}>
                 <img
