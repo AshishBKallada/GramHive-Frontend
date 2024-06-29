b3ab4d54d764d393e1d56bda9498e12cf50ff2fb
ASHISH B KALLADA <ashishbkallada@gmail.com>
2024-06-29 12:31:46 +0530

    story updation

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 import Swal from 'sweetalert2';
 
 const REFRESH_TOKEN_URL = 'https://bassheads.shop/auth/refresh-token'
-const BASE_URL = 'https://bassheads.shop'
+const BASE_URL = 'https://www.bassheads.shop'
 
 
 function getCurrentAccessToken() {
